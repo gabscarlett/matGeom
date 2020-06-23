2ba216c5a0ed783b6c1a6c89f1f866b95a9102ce
gabscarlett <gabriel.scarlett@mocean.energy>
2020-06-23 09:55:25 +0100

    Inhouse function to check if three points in a plane are collinear

---
+++ inHouseGeom/collinear.m
@@ -0,0 +1,19 @@
+   function tf = collinear(p1,p2,p3)
+   % test if three points are collinear in a plane
+   % make line with first two points
+     m = slope(p1,p2);
+     b = intercept(p1,p2);
+     % evaluate with third point
+     y = (m*p3(1)+b) ; 
+     % test if collinear within error bound
+     tf=abs(y- p3(2))<0.000001; % return true/false
+   end
+     % get slope
+     function m = slope(p1,p2)
+     m = (p2(2)-p1(2))/(p2(1)-p1(1));
+     end
+     % get intercept 
+     function b = intercept(p1,p2)
+    m = slope(p1,p2);
+    b = p1(2)-m*p1(1);
+   end
